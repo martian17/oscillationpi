dfbe17f280b3bc0f882d9dccedcbd76ae7b1c7fa
Yutaro Yoshii <yakumodanshi@gmail.com>
2021-03-25 22:33:19 +0900

    adding some descriptions

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
-var canvas = document.createElement("canvas");
+var canvas = document.getElementById("canvas");
 var width = 500;
-var height = 300;
+var height = 150;
 canvas.width = width;
 canvas.height = height;
-document.body.appendChild(canvas);
 var ctx = canvas.getContext("2d");
 
 var start = 0;
+var soffset = 0;
 
 var nlen = 200;
 var x = 300;
@@ -16,7 +16,7 @@ var m = 1;
 
 var N = -1//number times the weight has passed π/2
 
-//π=2t√(k/m) when x = n
+//π=2t√(k/m) when x(t) = n
 
 var drawSpring = function(){
     var coils = 10;
@@ -37,25 +37,35 @@ var drawWeight = function(x){
     ctx.fillRect(x,0,150,150);
 }
 
-var animate = function(t){
-    if(start === 0)start = t;
-    var dt = (start - t)/1000;
-    var tt = t/1000;
-    start = t;
+var calcPosition = function(tt,dt){
     var a = k/m*(nlen-x);
     //console.log(a);
     vx += a*dt;
     var x0 = x;
     x += vx*dt;
     if((x0-nlen)*(x-nlen) < 0){//crossed the line
-        N += 1;
-        var pi = 2*tt*Math.sqrt(k/m);
+        N += 2;
+        var pi = (2*tt*Math.sqrt(k/m))/N;
         console.log(pi);
+        document.getElementById("d2").innerHTML = "π = "+pi;
+    }
+};
+
+var animate = function(t){
+    if(start === 0){
+        start = t;
+        soffset = t;
+    }
+    var dt = (start - t)/1000;
+    var tt = (t-soffset)/1000;
+    start = t;
+    for(var i = 0; i < 1000; i++){
+        calcPosition(tt+i*dt/1000,dt/1000);
     }
     ctx.clearRect(0,0,width,height);
     drawSpring(x);
     drawWeight(x);
     requestAnimationFrame(animate);
-}
+};
 
 requestAnimationFrame(animate);
